9b07a3049801b058bfb3623ad0d1c239f613c2ce
Guohun Zhu <Guohun.Zhu.PhD@ieee.org>
2016-09-25 19:54:29 +1000

    point2D file

---
+++ Point2D.java
@@ -0,0 +1,11 @@
+package ChaosSignal;
+
+// Point2D.java
+
+public class Point2D
+{
+   public double x, y;
+   public Point2D() { x = 0; y = 0; }
+   public Point2D(double x0, double y0) { x = x0; y = y0; }
+   
+}
